fe498e92f90d636abcd9e60d5b0114363fc59f87
WellingtonRegis <ricardowellington854@gmail.com>
2023-08-30 15:20:31 -0300

    criando endpoints para buscar todos os usuario e usuario por Id

---
+++ src/main/java/com/ricardoregis/demonstracao/services/UserService.java
@@ -0,0 +1,30 @@
+package com.ricardoregis.demonstracao.services;
+
+import com.ricardoregis.demonstracao.entities.User;
+import com.ricardoregis.demonstracao.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
+
+
+}
