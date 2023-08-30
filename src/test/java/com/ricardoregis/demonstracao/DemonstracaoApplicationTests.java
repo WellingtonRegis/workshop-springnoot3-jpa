b502fbf4208a8533e4b4f31c646e12d1162beb29
WellingtonRegis <ricardowellington854@gmail.com>
2023-08-30 09:25:34 -0300

    criando teste

---
+++ src/test/java/com/ricardoregis/demonstracao/DemonstracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardoregis.demonstracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
