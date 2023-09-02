336f6accbac6d28ef0275a3b77bba925cd58a76a
WellingtonRegis <ricardowellington854@gmail.com>
2023-09-02 14:15:27 -0300

    implemento da classe OrderStatus, e correcoes de possiveis erros

--- src/main/java/com/ricardoregis/demonstracao/config/TestConfig.java
+++ src/main/java/com/ricardoregis/demonstracao/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.ricardoregis.demonstracao.config;
 
 import com.ricardoregis.demonstracao.entities.Order;
 import com.ricardoregis.demonstracao.entities.User;
+import com.ricardoregis.demonstracao.entities.enums.OrderStatus;
 import com.ricardoregis.demonstracao.repositories.OrderRepository;
 import com.ricardoregis.demonstracao.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Ricardo Regis", "ricardo@gmail.com", "99999-9999", "123456");
         User u2 = new User(null, "Paloma Santos", "paloma@gmail.com", "88888-8888", "654321");
 
-        Order o1 = new Order(null, Instant.parse("2023-09-25T05:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2023-09-15T08:43:15Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2023-09-17T13:38:37Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2023-09-25T05:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2023-09-15T08:43:15Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2023-09-17T13:38:37Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
